4b5683f16d06d77392a7e8f772bae881eb00a1ae
Vadim Gagarinskiy <vadim.gagarinskiy@wolterskluwer.com>
2020-09-30 08:34:11 -0700

    homework

---
+++ task1.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("task1.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task1.1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bd64c20-d468-4ed0-96db-d931444e10ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ task1.1/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RA;
+using System.Threading.Tasks;
+
+namespace task1._1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLaunch()
+        {
+            new RestAssured()
+                .Given()
+                .Param("page.page","1")
+                .Param("page.size", "1")
+                .Param("page.sort", "start_time,ASC")
+                .When()
+                .Get("https://rp.epam.com/ui/#vadim_gagarinskiy_personal/launches")
+                .Then()
+                .Debug()
+                .TestStatus("codeResponse", x => x == 200)
+                .AssertAll();
+        }
+
+        [TestMethod]
+        public void TestDashboardCreate()
+        {
+            new RestAssured()
+                .Given()
+                .Param("page.sort", "DASHBOARD NAME,ASC")
+                .When()
+                .Post("https://reportportal.epam.com/ui/#vadim_gagarinskiy_personal/dashboard")
+                .Then()
+                .Debug()
+                .TestStatus("codeResponse", x => x == 200)
+                .AssertAll();
+        }
+
+        [TestMethod]
+        public void TestDashboardEdit()
+        {
+            new RestAssured()
+                .Given()
+                .When()
+                .Put("https://reportportal.epam.com/ui/#vadim_gagarinskiy_personal/dashboard/14072")
+                .Then()
+                .Debug()
+                .TestStatus("codeResponse", x => x == 200)
+                .AssertAll();
+        }
+
+        [TestMethod]
+        public void TestDashboardDelete()
+        {
+            new RestAssured()
+                .Given()
+                .When()
+                .Delete("https://reportportal.epam.com/ui/#vadim_gagarinskiy_personal/dashboard/14072")
+                .Then()
+                .Debug()
+                .TestStatus("codeResponse", x => x == 200)
+                .AssertAll();
+        }
+    }
+}
