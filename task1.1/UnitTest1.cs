092d66a0ac92d33a93d58385e7abca282b69d5d8
Vadim Gagarinskiy <vadim.gagarinskiy@wolterskluwer.com>
2020-10-04 10:16:18 -0700

    homework with edit 4/10/2020

--- task1.1/UnitTest1.cs
+++ task1.1/UnitTest1.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RA;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace task1._1
 {
@@ -9,59 +13,120 @@ namespace task1._1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestLaunch()
+        public void TestAllLaunches()
         {
-            new RestAssured()
-                .Given()
-                .Param("page.page","1")
-                .Param("page.size", "1")
-                .Param("page.sort", "start_time,ASC")
-                .When()
-                .Get("https://rp.epam.com/ui/#vadim_gagarinskiy_personal/launches")
-                .Then()
-                .Debug()
-                .TestStatus("codeResponse", x => x == 200)
-                .AssertAll();
+            //1 all launches
+            var client = new RestClient("https://rp.epam.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("ui/#vadim_gagarinskiy_personal/launches/", Method.GET);
+            var queryResult = client.Execute<List<string>>(request).Data;
         }
 
         [TestMethod]
-        public void TestDashboardCreate()
+        public void TestAllLaunchesWhereTTLMoreThanNumber()
         {
-            new RestAssured()
-                .Given()
-                .Param("page.sort", "DASHBOARD NAME,ASC")
-                .When()
-                .Post("https://reportportal.epam.com/ui/#vadim_gagarinskiy_personal/dashboard")
-                .Then()
-                .Debug()
-                .TestStatus("codeResponse", x => x == 200)
-                .AssertAll();
+            //2 launches with TTL >20
+            const int totalExecutions = 20;
+            var client = new RestClient("https://rp.epam.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("ui/#vadim_gagarinskiy_personal/launches/{total_executions}", Method.GET);
+            request.AddParameter("total_executions", ">20");
+            var timer = new Stopwatch();
+            timer.Start();
+            var response = client.Execute<List<string>>(request);
+            timer.Stop();
+            
+            var queryResult = response.Data;
+            Console.WriteLine(queryResult);
+
+            //check StatusCode
+            Assert.AreEqual(response.StatusCode,200);
+
+            //3 check launches with status FAILED
+            int valueFailedStatus = 0;
+            foreach (var q in queryResult)
+            {
+                {
+                    //bool FAILED = check q.status == failed
+                }
+                //if(FAILED)
+                {
+                    valueFailedStatus += 1;
+                }
+            }
+
+            //7 assert that server's response < 1 second
+            Assert.IsTrue(timer.ElapsedMilliseconds<1000);
         }
 
         [TestMethod]
-        public void TestDashboardEdit()
+        public void TestCreateDashboard()
         {
-            new RestAssured()
-                .Given()
-                .When()
-                .Put("https://reportportal.epam.com/ui/#vadim_gagarinskiy_personal/dashboard/14072")
-                .Then()
-                .Debug()
-                .TestStatus("codeResponse", x => x == 200)
-                .AssertAll();
+            //4 create Dashboard
+            var client = new RestClient("https://rp.epam.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var requestBefore = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/", Method.GET);
+            var queryResultBefore = client.Execute<List<string>>(requestBefore).Data;
+
+            var request = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Item { });
+            client.Execute(request);
+
+            var requestAfter = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/", Method.GET);
+            var queryResultAfter = client.Execute<List<string>>(requestAfter).Data;
+            //Assert that dashboard list BEFORE POST method is not equal dashboard list AFTER POST method
+            Assert.AreNotEqual(queryResultBefore.Count,queryResultAfter.Count);
         }
 
         [TestMethod]
-        public void TestDashboardDelete()
+        public void TestEditDashboard()
         {
-            new RestAssured()
-                .Given()
-                .When()
-                .Delete("https://reportportal.epam.com/ui/#vadim_gagarinskiy_personal/dashboard/14072")
-                .Then()
-                .Debug()
-                .TestStatus("codeResponse", x => x == 200)
-                .AssertAll();
+            //5 edit Dashboard
+            int idDashboard = 5;
+            var client = new RestClient("https://rp.epam.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var requestBefore = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/{id}", Method.GET);
+            requestBefore.AddParameter("id", idDashboard);
+            var queryResultBefore = client.Execute<List<Item>>(requestBefore).Data;
+
+            var request = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/", Method.PUT);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Item { });
+            client.Execute(request);
+
+            var requestAfter = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/{id}", Method.GET);
+            requestAfter.AddParameter("id", idDashboard);
+            var queryResultAfter = client.Execute<List<Item>>(requestAfter).Data;
+
+            //Assert that dashboard's description with idDashboard ID BEFORE PUT method
+            //is not equal dashboard's description with idDashboard ID AFTER PUT method
+            Assert.AreNotEqual(queryResultBefore.Description, queryResultAfter.Description );
+        }
+
+        [TestMethod]
+        public void TestDeleteDashboard()
+        {
+            //6 delete Dashboard
+            int idDashboard = 5;
+            var client = new RestClient("https://rp.epam.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var requestBefore = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/", Method.GET);
+            var queryResultBefore = client.Execute<List<string>>(requestBefore).Data;
+
+            var request = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/{id}", Method.DELETE);
+            request.AddParameter("id", idDashboard);
+            client.Execute(request);
+
+            var requestAfter = new RestRequest("ui/#vadim_gagarinskiy_personal/dashboards/", Method.GET);
+            var queryResultAfter = client.Execute<List<string>>(requestAfter).Data;
+            //Assert that dashboard list BEFORE DELETE method is not equal dashboard list AFTER DELETE method
+            Assert.AreNotEqual(queryResultBefore.Count, queryResultAfter.Count);
         }
     }
 }
